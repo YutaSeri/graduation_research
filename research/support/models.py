7003ef98b147a2d341b4a67f3355662e0cf8bace
YutaSeri <s20b1031@bene.fit.ac.jp>
2023-08-07 23:22:47 +0900

    競合の解消

--- research/support/models.py
+++ research/support/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class p_support_Item(models.Model):
     item_name = models.CharField(verbose_name='物資一覧',max_length=255)
     quantity = models.IntegerField(verbose_name='個数')
-    created_at = models.DateTimeField(verbose_name='申請日',default=timezone.now, editable=False)
+    created_at = models.DateTimeField(verbose_name='申請日',default=timezone.now)
     arrival_date = models.DateTimeField(verbose_name='到着予定日',blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     phone_account = models.ForeignKey(phone_Account, on_delete=models.CASCADE)
@@ -16,3 +16,14 @@ class p_support_Item(models.Model):
     def __str__(self):
         return self.item_name
 # Create your models here.
+
+class Other_requests(models.Model):
+    requests  = models.TextField(verbose_name='その他の要望',max_length=255)
+    created_at = models.DateTimeField(verbose_name='投稿日',default=timezone.now, editable=False)
+    phone_account = models.ForeignKey(phone_Account, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'other_requests' 
+    
+    def __str__(self):
+        return self.requests
