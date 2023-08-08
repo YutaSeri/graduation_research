5ab8fcb234e2920f27d07dce9ddfeeeb719ae5f3
YutaSeri <s.yuta111@outlook.jp>
2023-08-08 13:36:58 +0900

    タブレット版立ち上げ

--- Tablet/Tablet/asgi.py
+++ Tablet/Tablet/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for research project.
+ASGI config for Tablet project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'research.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tablet.settings')
 
 application = get_asgi_application()
--- Tablet/manage.py
+++ Tablet/manage.py
@@ -4,10 +4,9 @@ import os
 import sys
 
 
-
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'research.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tablet.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
