9748ea0385ddd916fcd5ab3696952e6a274e2a8f
YutaSeri <s.yuta111@outlook.jp>
2023-08-08 16:06:41 +0900

    urlの設定

--- Tablet/tablet_app/urls.py
+++ Tablet/tablet_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-  
+  path('users/', views.user_detail_view, name='user_detail'),
 ]
--- Tablet/tablet_app/views.py
+++ Tablet/tablet_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def user_detail_view(request):
+    
+    return render(request, 'user_detail.html')
+
